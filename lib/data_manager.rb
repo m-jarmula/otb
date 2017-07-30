bff8bbf22b86a579b82c5eaa8a38a1d3a7fb880e
Maciej <maciej.jarmula@gmail.com>
2017-07-30 19:54:06 +0200

    DataManager refactor remove pointless get_index method and make insert_before and append private, they are not part of the interface

--- lib/data_manager.rb
+++ lib/data_manager.rb
@@ -5,25 +5,12 @@ class DataManager
     @jobs = []
   end
 
-  def get_index(job)
-    @jobs.index(job)
-  end
-
   def find_by_id(id)
     @jobs.find { |job| job.id == id }
   end
 
   def find_by_dependency(id)
     @jobs.find { |job| job.dependency == id }
-  end
-
-  def insert_before(before_node, job)
-    depended_job_index = get_index(before_node)
-    @jobs.insert(depended_job_index, job)
-  end
-
-  def append(job)
-    @jobs << job
   end
 
   def add(job)
@@ -33,5 +20,16 @@ class DataManager
     else
       append(job)
     end
+  end
+
+  private
+
+  def insert_before(before_node, job)
+    depended_job_index = @jobs.index(before_node)
+    @jobs.insert(depended_job_index, job)
+  end
+
+  def append(job)
+    @jobs << job
   end
 end
--- spec/data_manager_spec.rb
+++ spec/data_manager_spec.rb
@@ -5,8 +5,8 @@ describe 'DataManager' do
 
   describe '#append' do
     it 'adds element to the end' do
-      data_manager.append('a')
-      data_manager.append('b')
+      data_manager.send(:append, 'a')
+      data_manager.send(:append, 'b')
       expect(data_manager.jobs).to include('a', 'b')
       expect(data_manager.jobs.last).to eq('b')
     end
@@ -14,17 +14,10 @@ describe 'DataManager' do
 
   describe '#insert_before' do
     it 'adds elment before different element' do
-      data_manager.append('a')
-      data_manager.insert_before('a', 'b')
+      data_manager.send(:append, 'a')
+      data_manager.send(:insert_before, 'a', 'b')
       expect(data_manager.jobs).to include('a', 'b')
       expect(data_manager.jobs.first).to eq('b')
-    end
-  end
-
-  describe '#get_index' do
-    it 'returns index of given param' do
-      data_manager.append('a')
-      expect(data_manager.get_index('a')).to eq(0)
     end
   end
 
@@ -32,7 +25,7 @@ describe 'DataManager' do
     let(:job) { Job.new(id: 'b', dependency: 'a') }
 
     it 'searches for job by jobs dependency' do
-      data_manager.append(job)
+      data_manager.send(:append, job)
       expect(data_manager.find_by_dependency('a').id).to eq(job.id)
     end
   end
